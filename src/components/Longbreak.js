58d73d6eafc4ad6c4ea6f9f4d31d69f4f9e738ab
TiemaCoulibaly <47168830+TiemaCoulibaly@users.noreply.github.com>
2021-01-20 19:33:26 +0100

    trying add sound when counter down

--- src/components/Longbreak.js
+++ src/components/Longbreak.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles.css";
 const Longbreak = () => {
+	const [second, setSecond] = useState("00");
+	const [minute, setMinute] = useState("15");
+	const [counter, setCounter] = useState(0);
 	return (
 		<div id="longbreak">
-			<h2>je suis Longbreak</h2>
+			<h1>
+				<span>{minute}</span>
+				<span>:</span>
+				<span>{second}</span>
+			</h1>
+			<p>lorem ipsum dolor sit amet, consectetur</p>
 		</div>
 	);
 };
--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles.css";
 
 const Pomodoro = () => {
+	const [second, setSecond] = useState("00");
+	const [minute, setMinute] = useState("25");
+	const [counter, setCounter] = useState(5);
+	const [active, setActive] = useState(false);
+	useEffect(() => {
+		let intervalId;
+		if (active) {
+			intervalId = setInterval(() => {
+				const secondCounter = counter % 60;
+				const minuteCounter = Math.floor(counter / 60);
+
+				setSecond(secondCounter);
+				setMinute(minuteCounter);
+				setCounter((counter) => counter - 1);
+			}, 1000);
+		}
+		if (counter === 0) {
+			let ride = new Audio("../sounds/crash.mp3");
+			ride.play();
+			console.log(ride);
+
+			setSecond("00");
+			setMinute("00");
+			setCounter(0);
+		}
+		return () => clearInterval(intervalId);
+	}, [active, counter]);
+	// function handleSound(e) {
+	// 	e.preventDefault();
+	// 	let reek = new Audio("sounds/crash.mp3");
+	// 	reek.play();
+	// 	console.log("hey");
+	// }
+
 	return (
 		<div id="pomodoro">
-			<h3>Je suis le Pomodoro</h3>
+			<h1>
+				<span>{minute}</span>
+				<span>:</span>
+				<span>{second}</span>
+			</h1>
 			<p>lorem ipsum dolor sit amet, consectetur</p>
+			<button onClick={() => setActive(!active)}>Start</button>
+			<button>sound</button>
 		</div>
 	);
 };
